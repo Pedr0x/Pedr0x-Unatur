62bc16b99119457ca7a5ce75d15d6fb562adedc7
Pedr0x <43045235+Pedr0x@users.noreply.github.com>
2019-01-18 22:01:29 -0400

    Add files via upload

---
+++ Página-unatur/js/console-logo.js
@@ -0,0 +1,15 @@
+
+
+ function logo (){
+ 
+ console.log('%c Universidad Nacional del Turismo ', 'background: #fff; font-size:62px; color: #ffa000');
+
+ console.log('%c Diseñado por Pedro Joel Rodríguez ', 'background: #fff; font-size:42px; color: #00aeff');
+ 
+
+ }
+
+
+
+ 
+logo();
