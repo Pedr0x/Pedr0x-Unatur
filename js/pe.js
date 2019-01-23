263e0ce0860cee1bc2c3ff9d9aea44fde0ab4625
Pedr0x <43045235+Pedr0x@users.noreply.github.com>
2019-01-23 16:05:12 -0400

    Add files via upload
    Added JavaScript files

---
+++ js/pe.js
@@ -0,0 +1,71 @@
+var lenght1= 15;
+var width1= 10;
+var area1= lenght1  * width1;
+
+var lenght2 = 12;
+var width2 = 14;
+var area2= lenght2  *width3
+console.log(area1);
+
+function area (lenght, width){
+    console.log(lenght * width)
+}
+area(15,15)
+
+var rectanglesAreas = []
+rectanglesAreas.push(area(12,12));
+
+rectanglesAreas.push(area(10,14));
+rectanglesAreas.push(area(18,19));
+
+function makeTransaction (priceOfSale){
+    if (priceOfSale <= bankBalance){
+        console.log("Compra exitosa")
+    }
+    else {console.log("NO tienes plata")}
+}
+
+var printCustomerName = function (first, last){
+    consle.log("First Name:" + first + " Last name : " + last);
+}
+
+function student (first, last, age){
+    this.firstName = first;
+    this.lastName = last;
+    this.age= age;
+}
+
+ var student0 = {
+    firstName="Jayne";
+    lastName="Looo";
+    age:7;
+    greeting:function(){
+    return "Hi, I´m " + this.firstName + " and I´m " + this.age + " years old"
+}
+}
+var student ["Pedro" ,"José" "Koko","Pepe"];
+for (var index = 0; index < student.length; index++){
+    console.log(students{index});
+}
+console.log(student.lastName);
+
+var student2 = {};
+student2.firstName = "Zack";
+student2.lastName = "Bobo";
+student2.age = 5;
+
+var s1 = new student0 ("Jenny", "Laga", 5)
+console.log(s1);
+console.log(s1.greeting)
+
+/* var kaka = []
+students.push(student0)
+students.push(student2); */
+
+students.push(new student ("Jenny", "Laga"))
+students.push(new student)
+
+
+//Calculadora de porcentaje
+
+
